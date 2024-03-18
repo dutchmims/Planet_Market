1d546d6cae42dc2b37c92593bf844c133c65c6ab
dutchmims <jameshealy901@gmail.com>
2024-03-18 16:55:01 +0000

    Set up our database

--- planet_market/settings.py
+++ planet_market/settings.py
@@ -13,10 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -28,7 +28,6 @@ SECRET_KEY = 'django-insecure-p%*h$v%q-8-$9fu@1to(f(1fryp5s@h7+nn+(uj+fsca167n2n
 DEBUG = True
 
 ALLOWED_HOSTS = ['8000-dutchmims-planetmarket-cpi10scjff0.ws-eu110.gitpod.io']
-
 
 # Application definition
 
@@ -116,7 +115,6 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'planet_market.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -127,6 +125,9 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#    'default': dj_database_url.parse('postgres://hzxtjrwx:bfAdhchUDfcxQB_kKikxCxfy4HHfXzll@tyke.db.elephantsql.com/hzxtjrwx')
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
