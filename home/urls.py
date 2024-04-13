c119ca52f12730c274dcf7bb5534634d31ab9d25
dutchmims <jameshealy901@gmail.com>
2024-04-13 14:06:13 +0000

    Add url to 404 in my project folder

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from . import views
 
-# Import the custom 404 view function
 from .views import custom_page_not_found_view
 
 urlpatterns = [
     path('', views.index, name='home'),
 ]
 
-# Set the custom 404 error handler
 handler404 = 'home.views.custom_page_not_found_view'
