54149f0914d73eb3acb34f99caa7f69c3c400188
dutchmims <jameshealy901@gmail.com>
2024-04-13 13:56:22 +0000

    Add a 404 page

--- planet_market/views.py
+++ planet_market/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
 
-
-def index(request):
-
-    return render(request, 'home/index.html')
-
 def custom_page_not_found_view(request, exception):
-
     return render(request, '404.html', status=404)
