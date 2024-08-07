1fad8be41168d04f9610ed9abd46f44e82db5b6b
James Healy <jameshealy901@gmail.com>
2024-08-07 17:50:28 +0000

    Change db settings

--- planet_market/settings.py
+++ planet_market/settings.py
@@ -139,9 +139,9 @@ DATABASES = {
 
 # print("DATABASE_URL:", os.environ.get('DATABASE_URL'))
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
