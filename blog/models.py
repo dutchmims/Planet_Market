0f683f3f485e4c7e2f90d77fef9afffc2a6a085f
dutchmims <119881922+dutchmims@users.noreply.github.com>
2024-11-25 10:45:31 +0000

    Linted and formated the folder: /blog

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 # blog/models.py
-from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 # blog/views.py
-from django.shortcuts import render
 from .models import Post
 
 def index(request):
