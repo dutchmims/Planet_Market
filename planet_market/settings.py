ddd6cd146bed631d12a2c0ef4f0c90e7a541b03f
dutchmims <119881922+dutchmims@users.noreply.github.com>
2024-11-28 13:26:10 +0000

    Fix deploy issue: allowed hosts

--- planet_market/settings.py
+++ planet_market/settings.py
@@ -38,8 +38,9 @@ DEBUG = bool(os.getenv("DEBUG", "False"))
 if DEBUG:
     ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 else:
-    PRODUCTION_HOST_CI = os.getenv("PRODUCTION_HOSTS_CI", "")
-    PRODUCTION_HOSTS_HEROKU = os.getenv("PRODUCTION_HOSTS_HEROKU", "")
+    PRODUCTION_HOST_CI = os.getenv("PRODUCTION_HOSTS_CI",
+                                   "8000-dutchmims-planetmarket-bpjhkysy2er.ws.codeinstitute-ide.ne")
+    PRODUCTION_HOSTS_HEROKU = os.getenv("PRODUCTION_HOSTS_HEROKU", "planet-market-ef36a376b17d.herokuapp.com")
 
     ALLOWED_HOSTS = [PRODUCTION_HOST_CI, PRODUCTION_HOSTS_HEROKU]
 
