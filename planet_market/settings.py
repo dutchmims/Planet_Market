c2b4c95f1b68595bee11aedd827dc1ceb516c7aa
dutchmims <119881922+dutchmims@users.noreply.github.com>
2024-11-28 13:37:10 +0000

    Fix: debug was set to true .. now fix allowed hosts

--- planet_market/settings.py
+++ planet_market/settings.py
@@ -33,7 +33,7 @@ if env_path.exists():
 SECRET_KEY = os.getenv("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("DEBUG", "False"))
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
