faea1ea33e977528217c19751bbb0ee569b07f1d
dutchmims <119881922+dutchmims@users.noreply.github.com>
2024-12-01 17:12:43 +0000

    fix: debug for heroku

--- planet_market/settings.py
+++ planet_market/settings.py
@@ -20,7 +20,7 @@ if env_path.exists():
 SECRET_KEY = os.getenv("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Always include local development hosts
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
